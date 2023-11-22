bffa714a889001e21398afcec405835d385bbbbd
antonibote <antoni.bote@estudiantat.upc.edu>
2023-11-22 11:50:55 +0100

    Modified to handle A,B,C (%)

--- geoWithin.js
+++ geoWithin.js
@@ -4,10 +4,18 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = 'mongodb://127.0.0.1:27017';
 const dbName = 'greenguard';
 
+// Values sent by the frontend
+const B = 40 /* Set the value of B from the frontend */;
+const C = 20 /* Set the value of C from the frontend */;
+
+// Calculate new values
+const x = B + C / 2;
+const y = C;
+
 // Create a GeoJSON point
 const point = {
   type: 'Point',
-  coordinates: [99, 0]
+  coordinates: [x, y]
 };
 
 // Connect to MongoDB
@@ -17,20 +25,27 @@ MongoClient.connect(uri)
     const collection = db.collection('textureclasses');
 
     // Find all polygons
-    return collection.find({}).toArray();
-  })
-  .then((polygons) => {
-    // Check if the point is within any polygon
-    const foundPolygon = polygons.find((polygon) => {
-      const coordinates = polygon.geometry.coordinates.map(coord => [coord.x, coord.y]);
-      return isPointInPolygon(point.coordinates, coordinates);
-    });
+    return collection.find({}).toArray()
+      .then((polygons) => {
+        // Check if the point is within any polygon
+        const foundPolygon = polygons.find((polygon) => {
+          const coordinates = polygon.geometry.coordinates.map(coord => [coord.x, coord.y]);
+          return isPointInPolygon(point.coordinates, coordinates);
+        });
 
-    if (foundPolygon) {
-      console.log('Point is within polygon:', foundPolygon.name);
-    } else {
-      console.log('Point is not within any polygon.');
-    }
+        if (foundPolygon) {
+          console.log('Point is within polygon:', foundPolygon.name);
+        } else {
+          console.log('Point is not within any polygon.');
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        // Close the connection after use
+        client.close();
+      });
   })
   .catch((err) => {
     console.error(err);
